9d0a6ec044ff93d172086e34e7f1efa25b20d0ee
Lupti Wahyu <lupti.utama@advotics.com>
2025-07-16 22:25:45 +0700

    style: layout

--- news-app/src/App.tsx
+++ news-app/src/App.tsx
@@ -1,11 +1,51 @@
 import React from 'react'
 import './App.css'
+import { Layout } from 'antd'
+
+const { Header, Content, Footer } = Layout
 
 function App(): React.FC {
+  const currentYear: Number = new Date().getFullYear()
+
   return (
-    <>
-      <h1>Eigen News App</h1>
-    </>
+    <Layout>
+      <Header
+        style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 1,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: '20px',
+            fontWeight: 500,
+            margin: '0 auto',
+          }}
+        >
+          News App
+        </div>
+      </Header>
+      <Content style={{ padding: '48px' }}>
+        <div
+          style={{
+            padding: 24,
+            minHeight: 380,
+            background: 'white',
+            borderRadius: '8px',
+          }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Lupti Wahyu ©{currentYear}
+      </Footer>
+    </Layout>
   )
 }
 
